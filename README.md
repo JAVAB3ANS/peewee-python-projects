a8623afef299a22a30e6c5f5086175a5305f58f3
Jason Vu <jvu@scu.edu>
2021-07-16 00:40:28 -0700

    hi

---
+++ README.md
@@ -0,0 +1,2 @@
+# peewee-python-projects
+My assortment of completed and work-in-progress Python projects!
---
+++ bruteForce.py
@@ -0,0 +1,25 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+message = input("Enter secret message: ")
+
+def decode(secretMessage):
+    
+    #Try every key value
+
+    for key in range(len(alphabet)):
+        newAlphabet = alphabet[key:] + alphabet[:key]
+        attempt = ""
+
+        for i in range(len(secretMessage)):
+            index = alphabet.find(secretMessage[i])
+
+            if index < 0:
+                attempt += secretMessage[i]
+            else:
+                attempt += newAlphabet[index]
+        print("-"*15)
+        print("Key: " + str(key) + " - " + attempt)
+        print("-"*15)
+
+        #Call  decode function
+
+decode(message)
