ead4711cbc8ebd375bdee89d8477ec2609679fb1
Jason Vu <jvu@scu.edu>
2022-04-06 09:34:56 -0700

    edit caesarCipher.py

---
+++ caesarCipher/README.md
@@ -0,0 +1,17 @@
+# caesar-cipher
+
+<p align="center">
+    <img style="width: 75%" src="https://user-images.githubusercontent.com/59716037/115977215-b3484700-a52a-11eb-9808-d7cdb31ac660.png">
+</p>
+
+- Encrypt messages using Caesar's cipher by calling the `encrypt` function to encode the message.  
+```
+    Welcome to Caesar Cipher encryption and decryption:
+
+    Enter your message: CAESAR CIPHER EXAMPLE
+    Enter your shift: 4
+
+    Plain text: CAESAR CIPHER EXAMPLE
+    Shift pattern: 4
+    Cipher: HFJXFWsHNUMJWsJCFRUQJ
+```
--- caesarCipher/caesarCipher.py
+++ caesarCipher/caesarCipher.py
@@ -4,25 +4,30 @@ def encrypt (string, shift):
     # will store result of encryption
     encryption = ""
 
-    # loop through the length of the string
+    # traverse length of the string
     for i in range(len(string)): 
         if (string[i].isupper()):  
+            # lowercase letters of the string 
             encryption += chr((ord(string[i]) + shift - 64) % 26 + 65)
         else: 
+            # keep the same case as they're already lowercase
             encryption += chr((ord(string[i]) + shift - 96) % 26 + 97)
     
     return encryption
  
 def main():
-    print("Welcome to Caesar Cipher encryption and decryption: ")
+    print("Welcome to Caesar Cipher encryption and decryption:\n")
     
+    # message = str
     message_input = input("Enter your message: ")
+    # shift_input = int
     shift_input = int(input("Enter your shift: "))
 
+    # print if shift_input isn't int and if message_input isn't str
     if not type(shift_input) == int and not type(message_input) == str:
-        print("Please enter integer for shift input and/or string for message input")
+        print("\nPlease enter integer for shift input and/or string for message input")
 
-    print(f"Plain text: {message_input}")
+    print(f"\nPlain text: {message_input}")
     print(f"Shift pattern: {shift_input}")
     print(f"Cipher: {encrypt(message_input, shift_input)}")
 
